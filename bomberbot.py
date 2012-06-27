ed94795d678cd2eee148ad888c0af031f3df5671
Julian Vargas <julianvargaslvarez@gmail.com>
2012-06-26 21:36:56 -0500

    python client almost ready

---
+++ bomberbot.py
@@ -0,0 +1,41 @@
+import socket
+from bot import Bot
+
+class BomberBot():
+    def __init__(self):
+        try:
+            self.conectar("rbBot", "984198716")
+            self.controlConexion()
+        except Exception as e:
+            print(e)
+
+    def conectar(self, user, token):
+        self.socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	self.socket_cliente.connect(("localhost", 5000))
+        bienvenida = self.socket_cliente.recv(1279)
+        print(bienvenida)
+        self.socket_cliente.send("%s,%s" % (user, token))
+        self.conectado = True
+
+    def controlConexion(self):
+        response = None
+        while self.conectado:
+            print("turno")
+
+            server_message = self.socket_cliente.recv(511)
+            
+            message = server_message.split(";")
+
+            if message[0] == "EMPEZO":
+                bot = Bot(message[2][0])
+                bot.update_map(message[1])
+            elif message[0] == "TURNO":
+                print("turno: %s" % message[1])
+                bot.update_map(message[2])
+                msg = bot.move()
+                self.socket_cliente.send(msg)
+            elif message[0] == "PERDIO":
+                print("perdi :(")
+
+if __name__ == "__main__":
+	BomberBot()
---
+++ bot.py
@@ -0,0 +1,39 @@
+import random
+
+class Bot():
+
+    def __init__(self, letter):
+        self.letter = letter
+
+    def update_map(self, mapa):
+        rows = mapa.split('\n')
+        map = []
+        for index, row in enumerate(rows):
+            map[index] = row.split(",")
+
+        pos = mapa.index(self.letter) / 2
+        self.y = pos / rows.length
+        self.x = pos % rows.length
+
+    def move(self):
+        mov = random.randrange(0, 8)
+        print("mov %s" % mov)
+
+	if mov == 0:
+        	return "N"	
+	elif mov == 1:
+                return "E"
+        elif mov == 2:
+                return "S"
+        elif mov == 3:
+                return "O"
+	elif mov == 4:
+                return "BN"
+        elif mov == 5:
+                return "BE"
+        elif mov == 6:
+                return "BS"
+        elif mov == 7:
+                return "BO"
+
+        return "P"
